a0462eab98cee03540e996c822bfb2d15689cd15
Yunus AbdulMajid <Yunusabdulmajidyunus38@gmail.com>
2024-06-06 16:29:45 +0100

    styling

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,9 +4,10 @@ import { LuUser2 } from "react-icons/lu";
 import EditProfileForm from "../component/EditProfileForm";
 import { useUser } from "../hook/useStore";
 import Spinner from "../Ui/Spinner";
+import Wrapper from "../Ui/Wrapper";
 
 export default function Profile() {
-  const { name, photo, email,loading,fullName } = useUser();
+  const { name, photo, email, loading, fullName } = useUser();
   const [updateUser, setUpdateUser] = useState(false);
 
   // form visibility and hidden function
@@ -16,42 +17,40 @@ export default function Profile() {
   function cancleUpdateHandler() {
     setUpdateUser(false);
   }
-  if(loading) return <Spinner/>
+  if (loading) return <Spinner />;
   return (
     <>
-      <div className="pt-10 p-2 capitalize text-slate-50 flex gap-2 sm:gap-10 justify-center flex-col sm:flex-row">
-        {photo ? (
-          <img
-            src={photo}
-            alt={`${name} display photo`}
-            className="bg-gray-50 text-gray-950 w-60 h-60 grid items-center justify-center  font-NatoSans text-base font-bold uppercase"
-          />
-        ) : (
-          <LuUser2 className="text-gray-950 w-60 h-60 bg-gray-50" />
-        )}
-        <aside className="grid items-center">
-          <div className="flex gap-2 sm:flex-col">
-            <h1>name:</h1>
-            <span>{fullName}</span>
-          </div>
-          <div className="flex gap-2 sm:flex-col">
-            <h1>email:</h1>
-            <span className="lowercase">{email}</span>
-          </div>
-          <Button
-            style="px-4 bg-blue-600 w-fit disabled:cursor-not-allowed"
-            disabled={updateUser}
-            onClick={userUpdatehandler}
-          >
-            Edit profile
-          </Button>
-        </aside>
-      </div>
-      {updateUser && (
-        <div className="flex justify-center my-11 sm:my-0">
-          <EditProfileForm cancleUpdate={cancleUpdateHandler} />
+      <Wrapper>
+        <div className="pt-10 p-2 capitalize text-slate-50 flex gap-2 sm:gap-10 justify-center flex-col sm:flex-row">
+          {photo ? (
+            <img
+              src={photo}
+              alt={`${name} display photo`}
+              className="bg-gray-50 text-gray-950 w-60 h-60 grid items-center justify-center  font-NatoSans text-base font-bold uppercase"
+            />
+          ) : (
+            <LuUser2 className="text-gray-950 w-60 h-60 bg-gray-50" />
+          )}
+          <aside className="grid items-center">
+            <div className="flex gap-2 sm:flex-col">
+              <h1>name:</h1>
+              <span>{fullName}</span>
+            </div>
+            <div className="flex gap-2 sm:flex-col">
+              <h1>email:</h1>
+              <span className="lowercase">{email}</span>
+            </div>
+            <Button
+              style="px-4 bg-blue-600 w-fit disabled:cursor-not-allowed"
+              disabled={updateUser}
+              onClick={userUpdatehandler}
+            >
+              Edit profile
+            </Button>
+          </aside>
         </div>
-      )}
+      </Wrapper>
+      {updateUser && <EditProfileForm cancleUpdate={cancleUpdateHandler} />}
     </>
   );
 }
