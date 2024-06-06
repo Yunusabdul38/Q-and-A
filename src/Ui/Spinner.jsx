f06b7e99fe2a86fc1bb8544d5cb44ca979c8d854
Yunus AbdulMajid <Yunusabdulmajidyunus38@gmail.com>
2024-06-06 13:33:18 +0100

    modify spinner

--- src/Ui/Spinner.jsx
+++ src/Ui/Spinner.jsx
@@ -1,10 +1,7 @@
-import image from "../assets/apple-touch-icon.png"
 export default function Spinner() {
   return (
     <div className="flex justify-center items-center w-full h-svh bg-blue-900/25">
-      <div className="animate-spinner w-32 h-32 border-solid border-[5px] p-3">
-        <img src={image} alt="logo"/>
-      </div>
+      <div className="animate-spinner w-16 h-16 border-solid border-[5px]"></div>
     </div>
   );
 }
